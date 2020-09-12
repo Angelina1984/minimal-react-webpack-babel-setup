5af79c1f7b84304cbd3b8bcdf7e51a6dc8ddeb27
Angelina <rajna_ina@yahoo.com>
2020-09-12 13:29:43 -0700

    Update uploadCloudFormation.sh

--- jenkins/scripts/uploadCloudFormation.sh
+++ jenkins/scripts/uploadCloudFormation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 aws cloudformation create-stack --stack-name "$project.name-stack" \
-    --template-body file://../../aws/cloudformation/ecs.yml \
+    --template-body file://../../infrastructure/aws/cloudformation/ecs.yml \
     --region 'us-east-1' \
     --parameters ParameterKey=SubnetID,ParameterValue='subnet-09f3f7d31f77ca503' \
         ParameterKey=ServiceName,ParameterValue=project.name \
